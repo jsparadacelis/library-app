543649ab22e6a2cec008387fd40f85085dd83a4c
jsparadacelis <parada.celis@gmail.com>
2020-01-20 00:06:20 -0500

    add put method

--- api/views.py
+++ api/views.py
@@ -18,16 +18,29 @@ class BookList(generics.ListCreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            book_created = serializer.create(request.data)
-            if book_created == {}:
-                return Response(book_created, status=status.HTTP_400_BAD_REQUEST)
-            return Response(book_created, status=status.HTTP_201_CREATED)
+            book_saved = serializer.save(request.data)
+            if book_saved == {}:
+                return Response(book_saved, status=status.HTTP_400_BAD_REQUEST)
+            return Response(book_saved, status=status.HTTP_201_CREATED)
         return Response(None, status=status.HTTP_412_PRECONDITION_FAILED)
 
-class BookDetail(generics.RetrieveAPIView):
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
 
     serializer_class = BookSerializer
-    def retrieve(self, request, id, *args, **kwargs):
+    def get(self, request, id, *args, **kwargs):
         current_book = Book.objects.get(pk=id)
         serializer = self.get_serializer(current_book)
         return Response(serializer.data)
+
+    def put(self, request, id, *args, **kwargs):
+        book_data = {
+            'id': id
+        }
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            book_data = {**book_data, **request.data}
+            book_saved = serializer.save(book_data)
+            if book_saved == {}:
+                return Response(book_saved, status=status.HTTP_400_BAD_REQUEST)
+            return Response(book_saved, status=status.HTTP_200_OK)
+        return Response(None, status=status.HTTP_412_PRECONDITION_FAILED)
