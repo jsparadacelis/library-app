867d693936037d053d3fa3f8d0903d1ee2bebc58
jsparadacelis <parada.celis@gmail.com>
2020-01-19 22:49:58 -0500

    adding create method

--- api/views.py
+++ api/views.py
@@ -4,16 +4,25 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 # rest framework
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 # local
-from api.models import Book
+from api.models import Author, Book
 from api.serializers import BookSerializer
 
 
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            book_created = serializer.create(request.data)
+            if book_created == {}:
+                return Response(book_created, status=status.HTTP_400_BAD_REQUEST)
+            return Response(book_created, status=status.HTTP_201_CREATED)
+        return Response(None, status=status.HTTP_412_PRECONDITION_FAILED)
 
 class BookDetail(generics.RetrieveAPIView):
 
