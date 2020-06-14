f196d0586b2270a0ce44b7b2274838adbb9d887e
jsparadacelis <parada.celis@gmail.com>
2020-06-14 01:47:03 -0500

    add fixtures for testing book detail

--- api/tests.py
+++ api/tests.py
@@ -1,4 +1,7 @@
+from django.core.management import call_command
 from django.test import Client, TestCase
+
+from api import models
 
 
 class BookListTest(TestCase):
@@ -27,3 +30,16 @@ class BookListTest(TestCase):
             data_to_test
         )
         self.assertEqual(response.status_code, 400)
+
+class BookDetailTest(TestCase):
+
+    def setUp(self):
+        call_command('loaddata', 'fixtures/book_fixtures.yaml', verbosity=0)
+        self.client = Client()
+        self.book_path = '/book/'
+
+    def test_get_success(self):
+        response = self.client.get(
+            f'{self.book_path}'
+        )
+        self.assertEqual(response.status_code, 200)
