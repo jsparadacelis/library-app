e2a85752a3b45a2aa8595072698361d6855a685f
jsparadacelis <parada.celis@gmail.com>
2020-06-15 00:11:33 -0500

    add view to get a token for user

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from rest_framework.authtoken import views as auth_views
-
-from api.views import BookList, BookDetail
+from api.views import authetication, book
 
 admin_routes = [path('admin/', admin.site.urls), ]
 
-app_routes = [path('book/', BookList.as_view()), path('book/<int:id>', BookDetail.as_view())]
+app_routes = [
+    path('book/', book.BookList.as_view()),
+    path('book/<int:id>', book.BookDetail.as_view())
+]
 
-token_routes = [path('api-token-auth/', auth_views.obtain_auth_token)]
+token_routes = [
+    path('api-token-auth/', authetication.CustomAuthToken.as_view())
+]
 
 urlpatterns = admin_routes + app_routes + token_routes
